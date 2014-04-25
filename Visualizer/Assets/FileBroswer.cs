d4ff4820d8001f372b061e1104492ad94978e275
leurqum <mathieu.leurquin@epitech.eu>
2014-04-25 19:34:19 +0900

    Update FilesBrowser

---
+++ Visualizer/Assets/FileBroswer.cs
@@ -0,0 +1,34 @@
+﻿//----------------------------------------------
+//            NGUI: Next-Gen UI kit
+// Copyright © 2011-2013 Tasharen Entertainment
+//----------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Very basic script that will activate or deactivate an object (and all of its children) when clicked.
+/// </summary>
+
+[AddComponentMenu("NGUI/Interaction/Button Activate")]
+public class FileBroswer : MonoBehaviour
+{
+	public string message = "";
+	public float alpha = 1.0f;
+	public char pathChar = '/';
+	
+	void Start () {
+		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer) {
+			pathChar = '\\';
+		}
+	}
+	
+	void OnClick () {
+		UniFileBrowser.use.OpenFileWindow (OpenFile);
+	}
+	
+	void OpenFile (string pathToFile) {
+		var fileIndex = pathToFile.LastIndexOf (pathChar);
+		message = "You selected file: " + pathToFile.Substring (fileIndex+1, pathToFile.Length-fileIndex-1);
+	}
+	
+}
