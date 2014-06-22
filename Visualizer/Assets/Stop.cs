bcb60a8f479492034d2eeb719ea6e15c8c314488
leurqum <mathieu.leurquin@epitech.eu>
2014-06-22 13:53:44 +0900

    Menu in game + leave + time okay + name planet onlick
    Menu in game + leave + time okay + name planet onlick

---
+++ Visualizer/Assets/Stop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Stop : MonoBehaviour {
+
+	public GameObject manager;
+    public GameObject slider;
+    private float totalTime ;
+    // Use this for initialization
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void OnClick () {
+		UniverseTime w = manager.GetComponent<UniverseTime>();
+       	w.Reset();
+       	w.Start();
+	}
+}
